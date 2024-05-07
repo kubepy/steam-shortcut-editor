9090ab659a37dbd9f45b2dd2310cf986153468db
kubepy <43437760+kubepy@users.noreply.github.com>
2024-05-08 00:17:43 +0800

    Create json2vdf.js

---
+++ json2vdf.js
@@ -0,0 +1,13 @@
+cat input.js 
+const fs = require('fs');
+const shortcut = require('./lib');
+
+const filePath = './shortcuts.json';
+const writePath = './shortcuts_modified.vdf';
+
+writeBuffer = require(filePath);
+shortcut.writeFile(writePath, writeBuffer, function (err) {
+    if(err){
+        console.log('failed to write '+writePath);
+    }
+});
