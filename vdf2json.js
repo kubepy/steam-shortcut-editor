3b04124b6d2740f9d0682eee9d20596d9f1b5cf3
kubepy <43437760+kubepy@users.noreply.github.com>
2024-05-08 01:59:48 +0800

    Update vdf2json.js

--- vdf2json.js
+++ vdf2json.js
@@ -1,12 +1,8 @@
 const fs = require('fs');
-const config = {
-    steamUserId: process.env.STEAM_USER_ID,
-    steamDirectory: process.env.STEAM_DIRECTORY || '/home/deck/.local/share/Steam'
-};
 const shortcut = require('./lib');
 
-const filePath = config.steamDirectory + '/userdata/' + config.steamUserId + '/config/shortcuts.vdf';
-const writePath = './shortcuts.vdf';
+const filePath = './shortcuts.vdf';
+const WritePath = './shortcuts.json';
 
 shortcut.parseFile(filePath,
     { autoConvertArrays: true, autoConvertBooleans: true, dateProperties: ['LastPlayTime'] },
@@ -18,13 +14,7 @@ shortcut.parseFile(filePath,
             return;
         }
 
-        //Parsed Object
-        console.log(JSON.stringify(obj, null, 2));
-
-        shortcut.writeFile(writePath, obj, function (err) {
-            if(err){
-                console.log('failed to write '+writePath);
-            }
-        });
-
-    });
+        vdf_json=JSON.stringify(obj, null, 2);
+        console.log(vdf_json);
+        fs.writeFileSync(WritePath, vdf_json);
+});
